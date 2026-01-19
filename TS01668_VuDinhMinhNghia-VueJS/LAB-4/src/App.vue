ed163283a31cb66bb4d2ee55b63808ab5c202d3b
CindoSaur <gglau223@gmail.com>
2026-01-19 20:41:16 +0700

    Lab4 huhu

---
+++ TS01668_VuDinhMinhNghia-VueJS/LAB-4/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Hello from './components/Hello.vue';
+import Bai3 from './components/bai3.vue';
+import Bai4 from './components/bai4.vue';
+
+</script>
+
+<template>
+  <!-- <HelloWorld/> -->
+  <!-- <Hello/> -->
+  <!-- <bai3/> -->
+  <bai4 />
+</template>
+
+<style scoped>
+</style>
