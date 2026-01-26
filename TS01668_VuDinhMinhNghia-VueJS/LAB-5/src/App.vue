f05acad9ceb7ffc2e1826fe78feb01da1316bb6a
CindoSaur <gglau223@gmail.com>
2026-01-26 22:33:48 +0700

    Lab - 5 is done

---
+++ TS01668_VuDinhMinhNghia-VueJS/LAB-5/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+import CreatePost from './components/CreatePost.vue'
+import PostList from './components/PostList.vue'
+
+const posts = ref([])
+
+function addPost(post) {
+  posts.value.push(post)
+}
+</script>
+
+<template>
+  <div class="container mt-4">
+    <CreatePost @add-post="addPost" />
+    <PostList :posts="posts" />
+  </div>
+</template>
---
+++ TS01668_VuDinhMinhNghia-VueJS/LAB-5/src/components/Bai2.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-5">
+    <input v-model="userInput" class="form-control mb-3" placeholder="Nhập dữ liệu vào đâu" />
+    <p>Giá trị bạn nhập: {{ userInput }}</p>
+  </div>
+</template>
+<script setup>
+import {ref} from 'vue'
+const userInput=ref('FPL')
+</script>
