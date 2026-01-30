c04fac471a1addd2fb56ab8fc70d35e8838609bb
CindoSaur <gglau223@gmail.com>
2026-01-30 19:32:44 +0700

    DONE

--- TS01668_VuDinhMinhNghia-VueJS/LAB-6/src/App.vue
+++ TS01668_VuDinhMinhNghia-VueJS/LAB-6/src/App.vue
@@ -1,14 +1,21 @@
 <script setup>
 import Bai1 from './components/Bai1.vue';
 import Bai2 from './components/Bai2.vue';
-
+import Bai3 from './components/Bai3.vue';
+import Bai4 from './components/Bai4.vue';
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <Bai1/>
   </div> <br>
   <div>
     <Bai2/>
-  </div>
+  </div> -->
+<div>
+  <Bai3/>
+</div>
+<!-- <div>
+  <Bai4/>
+</div> -->
 </template>
