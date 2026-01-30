0415241b72f88c422c10d148f304e8dc021dbab6
CindoSaur <gglau223@gmail.com>
2026-01-30 16:50:51 +0700

    BAI 2 IS DONE!

--- TS01668_VuDinhMinhNghia-VueJS/LAB-6/src/App.vue
+++ TS01668_VuDinhMinhNghia-VueJS/LAB-6/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import Bai1 from './components/Bai1.vue';
+import Bai2 from './components/Bai2.vue';
 
 </script>
 
 <template>
   <div>
     <Bai1/>
+  </div> <br>
+  <div>
+    <Bai2/>
   </div>
-
 </template>
---
+++ TS01668_VuDinhMinhNghia-VueJS/LAB-6/src/components/Bai2.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>
+      Nhập thắng của bạn
+    </h3>
+    <input type="number" v-model-number="month"/>
+    <p v-if="month < 1 || month < 12">Vui lòng nhập tháng vào đây</p>
+    <p v-else-if="month >= 1 && month <= 3">Mù Xuân</p>
+    <p v-else-if="month >= 4 && month <= 6">Mù Hè</p>
+    <p v-else-if="motnh >= 7 && month <= 9">Mùa Thu</p>
+    <p v-else>Mù Đông</p>
+  </div>
+</template>
+<script setup>
+  import{ref} from 'vue';
+  const month = ref('1');
+</script>
