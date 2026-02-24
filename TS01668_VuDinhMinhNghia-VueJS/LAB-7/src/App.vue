bf7e530cdd145bbdbb24352a26d4bd0921db5df0
CindoSaur <gglau223@gmail.com>
2026-02-24 10:52:00 +0700

    LAB-7

---
+++ TS01668_VuDinhMinhNghia-VueJS/LAB-7/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// import Bai1 from './components/BAI1.vue';
+// import Bai2 from './components/BAI2.vue';
+// import Bai3 from './components/BAI3.vue';
+import Bai4 from './components/Bai4/Controller.vue';
+</script>
+
+<template>
+
+  <Bai1/>
+  <Bai2/>
+  <Bai3/>
+  <Bai4 />
+</template>
